b0b0013b28fe59514647988d94dfbebf824d8253
nbird33 <104222932+nbird33@users.noreply.github.com>
2023-03-21 21:19:18 -0400

    Add files via upload

---
+++ profitMarginCalc.java
@@ -0,0 +1,23 @@
+// Noah Bird
+// 2/14/18
+// find how much a business profits off a product if it operates on a 0.4 profit margin
+
+import java.util.Scanner;
+public class profitMargin1
+{
+	public static void main(String[] args)
+	{
+		double retail_price;
+		double profit;
+        double profit_margin = 0.4;
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.print("What is the retail price of the product? ");
+
+		retail_price = keyboard.nextDouble();
+
+		profit = retail_price * profit_margin;
+
+		System.out.println("Hello User, your profit is $" + profit );
+	}
+}
